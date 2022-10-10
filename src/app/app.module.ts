45a11427f2b372a3dc9b6e2b346d06e0619d8b13
aravindsabu <aravindsabu2001@gmail.com>
2022-10-10 20:05:45 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes =[
+  {
+    path:"",component:AddcourseComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddcourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
